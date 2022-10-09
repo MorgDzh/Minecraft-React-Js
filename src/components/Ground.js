6fa565e9bc7a07eb46f886e01c9458ba4281c9a0
Dzhanaliev Aktan <88595595+MorgDzh@users.noreply.github.com>
2022-10-10 03:14:18 +0600

    Added cube component :izakaya_lantern:

--- src/components/Ground.js
+++ src/components/Ground.js
@@ -1,24 +1,29 @@
 import { usePlane } from "@react-three/cannon";
-import { NearestFilter, RepeatWrapping } from "three";
 import { groundTexture } from "../images/textures";
+import { useStore } from "../hooks/useStore";
 
 const Ground = () => {
   const [ref] = usePlane(() => ({
     rotation: [-Math.PI / 2, 0, 0],
     position: [0, -0.5, 0],
   }));
+  const [addCube] = useStore((state) => [state.addCube]);
 
-  groundTexture.magFilter = NearestFilter
-  groundTexture.wrapS = RepeatWrapping
-  groundTexture.wrapT = RepeatWrapping
-  groundTexture.repeat.set(100, 100)
-  
+  groundTexture.repeat.set(100, 100);
+
   return (
-    <mesh ref={ref}>
+    <mesh
+      onClick={(e) => {
+        e.stopPropagation();
+        const [x, y, z] = Object.values(e.point).map((val) => Math.ceil(val));
+        addCube(x, y, z);
+      }}
+      ref={ref}
+    >
       <planeBufferGeometry attach="geometry" args={[100, 100]} />
       <meshStandardMaterial attach="material" map={groundTexture} />
     </mesh>
   );
 };
 
-export default Ground;
+export default Ground
