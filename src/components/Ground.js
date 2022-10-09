3189bc59eb5f088301c06cd128f336fc6b6ae3ea
Dzhanaliev Aktan <88595595+MorgDzh@users.noreply.github.com>
2022-10-09 22:41:42 +0600

    Added ground component and updated textures :zap:

--- src/components/Ground.js
+++ src/components/Ground.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import { usePlane } from "@react-three/cannon";
+import { groundTexture } from "../images/textures";
 
 const Ground = () => {
+  const [ref] = usePlane(() => ({
+    rotation: [-Math.PI / 2, 0, 0],
+    position: [0, -0.5, 0],
+  }));
   return (
-    <div>Ground</div>
-  )
-}
+    <mesh>
+      <planeBufferGeometry attach="geometry" args={[100, 100]} />
+      <meshStandardMaterial attach="material" map={groundTexture} />
+    </mesh>
+  );
+};
 
-export default Ground
+export default Ground;
