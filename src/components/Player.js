ac7fd640b724691fd6bcdd640f33783512d5b52d
Dzhanaliev Aktan <88595595+MorgDzh@users.noreply.github.com>
2022-10-09 23:01:00 +0600

    Added player component :rage4:

---
+++ src/components/Player.js
@@ -0,0 +1,33 @@
+import { useSphere } from "@react-three/cannon";
+import { useFrame, useThree } from "@react-three/fiber";
+import { useEffect, useRef } from "react";
+import { Vector3 } from "three";
+
+const Player = () => {
+  const { camera } = useThree();
+  const [ref, api] = useSphere(() => ({
+    mass: 1,
+    type: "Dynamic",
+    position: [0, 1, 0],
+  }));
+
+  const vel = useRef([0, 0, 0]);
+  useEffect(() => {
+    api.velocity.subscribe((v) => (pos.current = v));
+  }, [api.velocity]);
+
+  const pos = useRef([0, 0, 0]);
+  useEffect(() => {
+    api.position.subscribe((p) => (pos.current = p));
+  }, [api.position]);
+
+  useFrame(() => {
+    camera.position.copy(new Vector3(pos.current[0], pos.current[1]));
+
+    api.velocity.set(0, 1, 0)
+  });
+
+  return <mesh ref={ref}></mesh>;
+};
+
+export default Player;
